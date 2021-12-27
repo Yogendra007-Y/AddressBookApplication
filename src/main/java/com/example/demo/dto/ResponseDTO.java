cd9f03302c589b33e9848bda0e57a0eba5b031dd
Yogendra <you@example.com>
2021-12-27 19:15:10 +0530

    ADDED : Generation Of Getter And Setter Using Lombok Library

--- src/main/java/com/example/demo/dto/ResponseDTO.java
+++ src/main/java/com/example/demo/dto/ResponseDTO.java
@@ -1,5 +1,8 @@
 package com.example.demo.dto;
 
+import lombok.Data;
+
+@Data
 public class ResponseDTO {
 
 	private String message;
@@ -7,18 +10,6 @@ public class ResponseDTO {
 	public ResponseDTO(String message, Object data) {
 		super();
 		this.message = message;
-		this.data = data;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public Object getData() {
-		return data;
-	}
-	public void setData(Object data) {
 		this.data = data;
 	}
 	
