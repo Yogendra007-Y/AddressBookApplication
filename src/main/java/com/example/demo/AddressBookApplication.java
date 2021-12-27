6de86b873579780b7c4684b96b2b1c6cba2c4dcd
Yogendra <you@example.com>
2021-12-27 19:21:42 +0530

    ADDED : Lombok Library For Logging A File

--- src/main/java/com/example/demo/AddressBookApplication.java
+++ src/main/java/com/example/demo/AddressBookApplication.java
@@ -2,12 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Project : Address Book
+ * @author Yogendra Lalit Sharma
+ *
+ */
 @SpringBootApplication
+@Slf4j
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookApplication.class, args);
+		
+		ApplicationContext context = SpringApplication.run(AddressBookApplication.class, args);
+		log.info("Address Book App Started in {} Environment",context.getEnvironment().getProperty("environment"));
 	}
 
 }
